1f3584712c3bcd15af1b1ea21c72216e752d983a
Guilherme Robles <robles.guilherme@gmail.com>
2019-04-03 10:04:30 -0300

    Update Aplicacao.java

--- socialbooksapi-client/src/main/java/com/algaworks/socialbooks/aplicacao/Aplicacao.java
+++ socialbooksapi-client/src/main/java/com/algaworks/socialbooks/aplicacao/Aplicacao.java
@@ -10,7 +10,7 @@ import com.algaworks.socialbooks.client.domain.Livro;
 public class Aplicacao {
 	
 	public static void main(String[] args) throws ParseException {
-		LivrosClient cliente = new LivrosClient();
+		LivrosClient cliente = new LivrosClient("http://localhost:8080", "algaworks", "s3nh4");
 		
 		List<Livro> listaLivros = cliente.listar();
 		
@@ -30,5 +30,10 @@ public class Aplicacao {
 		String localizacao = cliente.salvar(livro);
 		
 		System.out.println("URI do livro salvo: " + localizacao);
+		
+		Livro livroBuscado = cliente.buscar(localizacao);
+		
+		System.out.println("Livro buscado: " + livroBuscado.getNome());
+		
 	}
 }
